979f24ba6bb8d4d96e2932b85615db4af5ac9275
Mathis Vialle <mathis.vialle.etudiant@gmail.com>
2021-12-01 11:28:24 +0100

    Introduction de webpack

---
+++ bundler/webpack.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html')
+        })
+    ],
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    module: {
+        rules: [
+            // PARTIE HTML
+            {
+                test: /\.html$/i,
+                use: ["html-loader"]
+            },
+            // PARTIE CSS
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            //PARTIE IMAGES
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
