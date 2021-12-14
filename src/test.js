0c041e91453deeb7f241dcaec0057d9a44881bfc
Mathis Vialle <mathis.vialle.etudiant@gmail.com>
2021-12-14 15:07:16 +0100

    ☮

--- src/test.js
+++ src/test.js
@@ -1,15 +1,33 @@
+    /* 
+        Etapes :
+            1. Recup les points de la timeline, le num d'étape et le nom d'étape
+            2. Tester le numéro d'étape --> Créer variable ? Changer les ID de num et nom d'étape ? (valable que avec incrémentation)
+            3. Changer en fonction du num d'étapes les éléments recups précédement
+            4. Incrémenter le numéro d'étape en fonction de la différence entre le précédent et l'actuel
+    */
+
 function test() {
-    let test = document.querySelectorAll(".timeline__point");
-    console.log(test)
+    let timeline = document.querySelectorAll(".timeline__point")
+    let numEtape = document.querySelector(".num-etape")
+    let nomEtape = document.querySelector(".nom-etape")
+
+    console.log(timeline)
+
+ /*   var etapes = [
+        {
+             : 
+        }
+
+    ]
     
-    test.forEach (function(el, i) {
+    timeline.forEach (function(el, i) {
 
         if (el.classList.contains("-actif")) {
-
-            console.log(el);
-
             el.innerHTML = '<img src="./assets/Etoile.svg" alt="Etoile étape actif"><br/><h4>Milieu</h4>'
+        } 
+        else {
+            el.innerHTML = '<img src="./assets/Icones/eclipse.svg" alt="ellipse étape">'
+        }      
 
-        }
-    })
+    }) */
 }
